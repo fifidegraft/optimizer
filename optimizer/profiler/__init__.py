118ef1dbe72639485367ff998a23d486132eb6d6
fifi degraft <gfd35@cornell.edu>
2026-09-12 22:28:09 -0400

    Revert "Merge pull request #11 from fifidegraft/profiler"
    This reverts commit 2d9328aa725e6099b8aa1a297127871b5979efa9, reversing
    changes made to f7b95a256d2d0139cd8657fdc55066c8aed12b37.

--- optimizer/profiler/__init__.py
+++ optimizer/profiler/__init__.py
@@ -1,6 +1,5 @@
 """Profiler module — Performance measurement and profiling."""
 
 from .runner import run_workload, benchmark_workload
-from .profiler import profile_workload
 
-__all__ = ["run_workload", "benchmark_workload", "profile_workload"]
+__all__ = ["run_workload", "benchmark_workload"]
